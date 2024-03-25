d39acbdad3b756f87b5e6e4231dd9f918a62baa5
Daniel Seitenfus <daniel.seitenfus@combateafraude.com>
2024-03-25 07:47:12 -0300

    feat(ImageEditor): create Button

--- Image Editor/T1DanielSeitenfus/src/Button.h
+++ Image Editor/T1DanielSeitenfus/src/Button.h
@@ -2,29 +2,31 @@
 #define __BOTAO_H__
 
 #include "gl_canvas2d.h"
+#include "Color.h"
 
-class Botao{
+class Button {
   float altura, largura, x, y;
   char label[100];
+  Color buttonColor;
+  Color textColor;
+
+  //std::function<void()> action;
 
 public:
-  Botao(float _x, float _y, float _larg, float _alt, char *_label) {
-     altura  = _alt;
-     largura = _larg;
-     x = _x;
-     y = _y;
+  Button(float _x, float _y, float _larg, float _alt, char *_label, Color _buttonColor, Color _textColor)
+      : x(_x), y(_y), largura(_larg), altura(_alt), buttonColor(_buttonColor), textColor(_textColor) {
      strcpy(label, _label);
   }
 
-  void Render() {
-      CV::color(0, 1, 0);
+  void render() {
+      CV::color(buttonColor.r, buttonColor.g, buttonColor.b);
       CV::rectFill(x, y, x + largura, y + altura);
-      CV::color(0, 0, 0);
+      CV::color(textColor.r, textColor.g, textColor.b);
       CV::text(x+5, y+altura/2, label); //escreve o label do botao mais ou menos ao centro.
   }
 
   //recebe as coordenadas do mouse para tratar clique ou detectar quando o mouse esta em cima do botao
-  bool Colidiu(int mx, int my) {
+  bool colidiu(int mx, int my) {
       return mx >= x && mx <= (x + largura) && my >= y && my <= (y + altura);
   }
 
